27163ed461483414de0f53bdef07df4c4c2ddac3
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-08-12 19:27:33 +0700

    UPDATE FILE TEMP FORMAT

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,34 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'cbtonline_db',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '34.128.64.255',
-	'username' => 'root',
-	'password' => 'Rdss1995',
-	'database' => 'cbtonline-db',
+	'hostname' => 'srv144.niagahoster.com',
+	'username' => 'webd1830_cbtonline_db',
+	'password' => 'smktkjTH46',
+	'database' => 'webd1830_cbtonline_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
