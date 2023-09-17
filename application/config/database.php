3f138a6afd98d07c67d5d7fc186bc0ce61834cc1
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-09-17 16:40:08 +0700

    MVC UPDATE DB

--- application/config/database.php
+++ application/config/database.php
@@ -27,37 +27,12 @@ $query_builder = TRUE;
 // 	'save_queries' => TRUE
 // );
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'srv144.niagahoster.com',
-	'username' => 'webd1830_cbtonline_db',
-	'password' => 'smktkjTH46',
-	'database' => 'webd1830_cbtonline_db',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-
-
 // $db['default'] = array(
-// 	'port' => 16751,
 // 	'dsn'	=> '',
-// 	'hostname' => 'vultr-prod-c0653573-c574-4def-ac2e-13330230b590-vultr-prod-4a64.vultrdb.com',
-// 	'username' => 'vultradmin',
-// 	'password' => 'AVNS_Q3RIERmpf9xGGnfrj1r',
-// 	'database' => 'cbtonline_db',
+// 	'hostname' => 'srv144.niagahoster.com',
+// 	'username' => 'webd1830_cbtonline_db',
+// 	'password' => 'smktkjTH46',
+// 	'database' => 'webd1830_cbtonline_db',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -69,7 +44,32 @@ $db['default'] = array(
 // 	'swap_pre' => '',
 // 	'encrypt' => FALSE,
 // 	'compress' => FALSE,
-// 	//'stricton' => FALSE,
+// 	'stricton' => FALSE,
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+
+
+
+$db['default'] = array(
+	'port' => 16751,
+	'dsn'	=> '',
+	'hostname' => 'vultr-prod-c0653573-c574-4def-ac2e-13330230b590-vultr-prod-4a64.vultrdb.com',
+	'username' => 'vultradmin',
+	'password' => 'AVNS_Q3RIERmpf9xGGnfrj1r',
+	'database' => 'cbtonline_db',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	//'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
