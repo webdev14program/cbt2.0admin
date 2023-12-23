74bc401f179276944fff7d08bf6f30aef3eeb2fe
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-12-23 15:52:01 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -27,37 +27,12 @@ $query_builder = TRUE;
 // 	'save_queries' => TRUE
 // );
 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'srv144.niagahoster.com',
-// 	'username' => 'webd1830_cbtonline_db',
-// 	'password' => 'smktkjTH46',
-// 	'database' => 'webd1830_cbtonline_db',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
-
-
-
 $db['default'] = array(
-	'port' => 16751,
 	'dsn'	=> '',
-	'hostname' => 'vultr-prod-9d02be44-4f81-491e-a4c3-253f9135a26a-vultr-prod-9992.vultrdb.com',
-	'username' => 'vultradmin',
-	'password' => 'AVNS_p1jDCFybgNKM0TjWDJY',
-	'database' => 'cbt_db_susulan',
+	'hostname' => 'srv144.niagahoster.com',
+	'username' => 'webd1830_cbtonline_db',
+	'password' => 'smktkjTH46',
+	'database' => 'webd1830_cbtonline_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -69,7 +44,32 @@ $db['default'] = array(
 	'swap_pre' => '',
 	'encrypt' => FALSE,
 	'compress' => FALSE,
-	//'stricton' => FALSE,
+	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
+
+// $db['default'] = array(
+// 	'port' => 16751,
+// 	'dsn'	=> '',
+// 	'hostname' => 'vultr-prod-9d02be44-4f81-491e-a4c3-253f9135a26a-vultr-prod-9992.vultrdb.com',
+// 	'username' => 'vultradmin',
+// 	'password' => 'AVNS_p1jDCFybgNKM0TjWDJY',
+// 	'database' => 'cbt_db_susulan',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	//'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
