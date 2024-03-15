a8f647ef769bfa69c022167a94656eb2ab06be29
Rahmadika S Setiawan <dikakewren@outlook.com>
2024-03-15 20:51:37 +0700

    MVC UPDATE STS GHENAP FIX

--- application/config/database.php
+++ application/config/database.php
@@ -7,9 +7,9 @@ $query_builder = TRUE;
 
 // $db['default'] = array(
 // 	'dsn'	=> '',
-// 	'hostname' => '192.168.1.10',
-// 	'username' => 'cbt',
-// 	'password' => 'smktkjTH46',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
 // 	'database' => 'cbtonline_db',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
@@ -27,31 +27,31 @@ $query_builder = TRUE;
 // 	'save_queries' => TRUE
 // );
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'srv144.niagahoster.com',
-	'username' => 'webd1830_cbtonline_db',
-	'password' => 'smktkjTH46',
-	'database' => 'webd1830_cbtonline_db',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'srv144.niagahoster.com',
+// 	'username' => 'webd1830_cbtonline_db',
+// 	'password' => 'smktkjTH46',
+// 	'database' => 'webd1830_cbtonline_db',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
 
 // $db['default'] = array(
 // 	'dsn'	=> '',
-// 	'hostname' => '192.168.1.19',
+// 	'hostname' => '192.168.1.24',
 // 	'username' => 'cbt',
 // 	'password' => 'smktkjTH46',
 // 	'database' => 'cbtonline_db',
@@ -95,25 +95,24 @@ $db['default'] = array(
 
 
 
-// $db['default'] = array(
-// 	'port' => 3306,
-// 	'dsn'	=> '',
-// 	'hostname' => '149.28.141.61',
-// 	'username' => 'cbt',
-// 	'password' => 'smktkjTH46',
-// 	'database' => 'cbtonline_db_sas_fix4_hari4',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	//'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '108.61.96.35',
+	'username' => 'cbt',
+	'password' => 'smktkjTH46',
+	'database' => 'cbtonline_db_fix',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
